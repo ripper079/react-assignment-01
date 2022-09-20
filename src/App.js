9e27d1495dc3afb9954f35c764d77e7087ef55e6
Daniel Oikarainen <powerdeveloper@fastmail.se>
2022-09-20 10:32:27 +0200

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>App Powered by React!</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+{/*         
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,50 @@ function App() {
         >
           Learn React
         </a>
+         */}
+
+        <form method="post" class="form-control">
+        <h2>Insert Person data</h2>
+            {/*                                     
+            <p>
+                <label>First Name: <input type="text" name="humantemperature" /></label>
+            </p>
+            <p>
+                <button type="submit">Submit</button>
+            </p>     */}
+        
+        <div className="form-group">
+          <label>First name: </label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Second name: </label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Age: </label>
+          <input type="number" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Nationality: </label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Email: </label>
+          <input type="email" className="form-control" />
+        </div>
+
+        <input type="submit" value="Add a Person" className="btn btn-primary" />
+
+        
+        </form>         
+
+
+
       </header>
     </div>
   );
